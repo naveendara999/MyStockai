43e5fccc94ffaf9c431d2b7156d6ceda43cecad3
Naveen Kumar <naveen.kumar@celebaltech.com>
2021-07-13 20:11:53 +0530

    update redux

--- src/redux/actions/actionTypes.js
+++ src/redux/actions/actionTypes.js
@@ -1,3 +1,10 @@
-export const LOGIN = 'LOGIN';
-export const LOGOUT = 'LOGOUT';
-export const SIGNUP = 'SIGNUP';
+import {
+  errorAction,
+  loadingAction,
+  successSideEffectState,
+  SideEffectSchema,
+} from ".";
+
+export const LOGIN = "LOGIN";
+export const LOGOUT = "LOGOUT";
+export const SIGNUP = "SIGNUP";
